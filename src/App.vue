b19dc142ee6fe1a0dee1ad888f205e0e3917e8fe
Ramon Noleto <devnoleto@gmail.com>
2024-09-13 16:37:24 -0300

    :tada: radix-vue: Instalação do Radix-vue
    Instalação do Radix-vue e criação do um componente modelo Popover usando o radix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+import Popover from './components/ui/Popover.vue';
 </script>
 
 <template>
   <div>
     <p>Conteudo de App.vue</p>
+    <hr>
+    <Popover />
   </div>
 </template>
-
