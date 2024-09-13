2b9bb06d666eccd6ec84ab65c3c3138e9968580e
Ramon Noleto <devnoleto@gmail.com>
2024-09-13 16:38:06 -0300

    Revert ":tada: radix-vue: Instalação do Radix-vue"
    This reverts commit b19dc142ee6fe1a0dee1ad888f205e0e3917e8fe.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import Popover from './components/ui/Popover.vue';
+
 </script>
 
 <template>
   <div>
     <p>Conteudo de App.vue</p>
-    <hr>
-    <Popover />
   </div>
 </template>
+
