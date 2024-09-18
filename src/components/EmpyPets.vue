6129efddeae4d6875354d5d4eb0c5826140c7e30
Ramon Noleto <devnoleto@gmail.com>
2024-09-18 13:40:29 -0300

    :tada: component: Ajuste de logo
    Criação de logo para usar na página incial

--- src/components/EmpyPets.vue
+++ src/components/EmpyPets.vue
@@ -2,12 +2,13 @@
 import Dialog from './ui/Dialog.vue';
 import { DialogTrigger } from 'radix-vue';
 import letsStart from '../assets/lets-start-illustration.svg'
+import logoAdopets from '../assets/Adopets.svg'
 </script>
 
 <template>
     <div class="h-screen flex flex-col items-center justify-center gap-8">
-        <img src="" alt="logo-adopets">
-        <img :src="letsStart" class="" alt="adopets">
+        <img :src="logoAdopets" alt="logo-adopets" class="w-[80px]">
+        <img :src="letsStart" alt="adopets">
         <p class="text-zinc-300 leading-relaxed max-w-80 text-center">Você ainda não cadastrou nenhum Pet</p>
         <Dialog>
             <DialogTrigger>
