01d4a2f31fe4ee1c9d1eff699bfe273f9b9bc835
Ramon Noleto <devnoleto@gmail.com>
2024-09-20 14:06:22 -0300

    :save: bkp: BKP de ajuste 2
    Ajustando formulario para salvar infos do pet no banco de dados

--- src/components/ui/Form.vue
+++ src/components/ui/Form.vue
@@ -2,103 +2,109 @@
 import { ref } from 'vue';
 import { DialogClose } from 'radix-vue';
 import axios from 'axios';
+import dayjs from 'dayjs';
 
+// Componentes importados
 import TextInput from './TextInput.vue';
 import DatePickerInput from './DatePickerInput.vue';
 import Button from './Button.vue';
-
 import RadioGroup from './RadioGroup.vue';
 
-const petName = ref('')
-const petChip = ref('')
-const birthDate = ref('')
-const breedPet = ref('')
-const speciePet = ref('')
-const genderPet = ref('')
-const isCastrated = ref('')
-const petSize = ref('')
+// Variáveis reativas para armazenar os dados do formulário
+const petData = ref({
+  name: '',
+  chip_number: '',
+  birth: '01/12/2019',
+  specie: 'Canina', // Especie padrão definida como "Canina"
+  breed: '',
+  gender: 'F', // Sexo padrão definido como "F"
+  status: 1,
+  ref_id_user: 1, // ID do usuário
+  ativo: 1 // Ativo padrão
+});
 
+// Função para salvar os dados do pet
 const savePet = async () => {
-  try{
-    const response = await axios.post('/animals', {
-      name: petName.value,
-      chip_number: petChip.value,
-      birth: birthDate.value,
-      specie: speciePet.value,
-      breed: breedPet.value,
-      gender: genderPet.value,
-      status: 1,
-      ref_id_user: 1
-    });
+  // Garantir que a data está no formato YYYY-MM-DD
+  petData.value.birth = dayjs(petData.value.birth).format('YYYY-MM-DD');
 
+  try {
+    const response = await axios.post('/animals', petData.value);
     console.log('Pet cadastrado com sucesso:', response.data);
-
-  }catch(error){
-    console.error('Erro ao cadastrar o Pet: ', error);
+  } catch (error) {
+    console.error('Erro ao cadastrar o Pet:', error.response.data);
   }
-}
+};
 </script>
 
 <template>
   <div class="mt-4 flex flex-col gap-4 h-full">
     <div class="flex flex-col gap-4">
       <!-- Campo de Nome do Pet -->
-      <TextInput label="Nome do Pet" id="petName" v-model="petName"/>
-      <!-- Numero do Chip -->
-      <TextInput type="number" label="Nº do Chip" id="petChip" v-model="petChip"/>
-      <!-- Especie do Pet -->
-       <RadioGroup 
-        groupLabel="Especie do Pet"
-        v-model="speciePet"
+      <TextInput label="Nome do Pet" id="petName" v-model="petData.name" required />
+      
+      <!-- Campo de Nº do Chip -->
+      <TextInput type="number" label="Nº do Chip" id="petChip" v-model="petData.chip_number" />
+      
+      <!-- Campo de Espécie do Pet -->
+      <RadioGroup 
+        groupLabel="Espécie do Pet"
+        v-model="petData.specie"
         :options="[
-          { label: 'Cachorro', value: 'dog' },
-          { label:'Gato', value: 'cat'}
+          { label: 'Canina', value: 'Canina' },
+          { label:'Felina', value: 'Felina' }
         ]"
-        defaultValue="dog"
-       />
+        defaultValue="Canina"
+      />
+      
       <!-- Campo de Raça do Pet -->
-      <TextInput label="Raça do Pet" id="breedPet" v-model="breedPet"/>
+      <TextInput label="Raça do Pet" id="breedPet" v-model="petData.breed" required />
+      
       <!-- Campo de Data de Nascimento do Pet -->
-      <DatePickerInput label="Data de Nascimento" id="birthDate" v-model="birthDate" />
-      <!-- Campo para Sexo do Pet -->
+      <DatePickerInput label="Data de Nascimento" id="birthDate" v-model="petData.birth" required />
+      
+      <!-- Campo de Sexo do Pet -->
       <RadioGroup
         groupLabel="Sexo do Pet"
-        v-model="genderPet" 
+        v-model="petData.gender"
         :options="[
-          { label: 'Macho', value: 'male'}, 
-          {label: 'Fêmea', value: 'female'}
-          ]" 
-          defaultValue="male" 
+          { label: 'Macho', value: 'M' }, 
+          { label: 'Fêmea', value: 'F' }
+        ]"
+        defaultValue="F"
+        required
       />
-      <!-- Castrado? -->
-       <RadioGroup
-          groupLabel="Castrado"
-          v-model="isCastrated"
-          :options="[
-            { label: 'Sim', value: 'S'},
-            { label: 'Não', value: 'N'}
-          ]"
-          defaultValue="N"
-       />
-      <!-- Campo para Porte do Pet -->
+      
+      <!-- Campo para Castrado -->
       <RadioGroup
-        groupLabel="Tamanho do Pet" 
-        v-model="petSize" 
+        groupLabel="Castrado"
+        v-model="petData.is_castrated"
         :options="[
-          { label: 'Pequeno', value: 'small'},
+          { label: 'Sim', value: 'S' },
+          { label: 'Não', value: 'N' }
+        ]"
+        defaultValue="N"
+      />
+      
+      <!-- Campo para Tamanho do Pet -->
+      <RadioGroup
+        groupLabel="Tamanho do Pet"
+        v-model="petData.pet_size"
+        :options="[
+          { label: 'Pequeno', value: 'small' },
           { label: 'Médio', value: 'medium' },
           { label: 'Grande', value: 'large' }
         ]"
         defaultValue="small"
       />
     </div>
-    <!-- Campo de Botões -->
+    
+    <!-- Botões de ação -->
     <div class="flex justify-between gap-4">
-      <Button variant="primary" size="default" class="w-full" @click='savePet'>Cadastrar Pet</Button>
+      <Button variant="primary" size="default" class="w-full" @click="savePet">Cadastrar Pet</Button>
       <DialogClose>
         <Button variant="secondary" class="w-36">Cancelar</Button>
       </DialogClose>
     </div>
   </div>
-  
 </template>
