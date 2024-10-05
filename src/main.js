ff817acf4f2e0c1d7494822eb24a4d702c64627d
Ramon Noleto <ramon.zap@hotmail.com>
2024-10-05 12:56:47 -0300

    :spark: test: Rota Vercel
    Teste de endpoint api da vercel

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import router from './router'
 import axios from 'axios'
 
 const storageBaseUrl = 'http://127.0.0.1:8000/storage/';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+axios.defaults.baseURL = 'https://adopets-api.vercel.app/api/api/';
 axios.storageBaseUrl = storageBaseUrl;
 
 const pinia = createPinia();
